5d16167cc9de2276c7074bbb4ccb725bc8827196
Alejandromm1227 <160982660+Alejandromm1227@users.noreply.github.com>
2024-05-22 20:10:09 -0500

    express

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+"use strict";
 console.log("works!");
+
+const express = require("express");
+
+const servidor = express();
+
+servidor.get("/",(solicitud, respuesta)=>{
+    respuesta.json({
+        saludo : "hola"
+    })
+});
+
+servidor.listen(3000);
+console.log("servidor 3000");
